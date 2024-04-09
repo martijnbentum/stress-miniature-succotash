c3617c917328d7539b5595734d13ceb4e2093cff
martijnbentum <bentummartijn@gmail.com>
2024-04-09 14:58:12 +0200

    renamed database to text

--- stress/settings.py
+++ stress/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b-w^__g=4nl&^s_6=kqj@ctv#ur+=rbt$8xw0-6zyj+t4rhdo='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'text.apps.TextConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
