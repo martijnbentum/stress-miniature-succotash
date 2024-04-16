279957351bde913981c1b07e9ed595c5885066ba
martijnbentum <bentummartijn@gmail.com>
2024-04-16 22:26:03 +0200

    move bpc to seperate module

--- utils/load_cgn_phonemes.py
+++ utils/load_cgn_phonemes.py
@@ -1,37 +1,6 @@
 from utils import load_cgn_textgrids
 
 
-def make_bpcs():
-    '''make a BPCs instance with all broad phonetic classes.'''
-    names = 'plosive,nasal,approximant,fricative,voiced,voiceless'
-    names += ',high_vowel,mid_vowel,low_vowel,front_vowel,central_vowel'
-    names += ',back_vowel,close_vowel,open_vowel'
-    names = names.split(',')
-    bpcs = 'p t k b d g,n m ŋ ɲ,l r j w ʋ,s ʃ ʒ h f x z v ɣ'
-    bpcs += ',b d g m n ŋ ɲ l r j w ʋ ʒ z v'
-    bpcs += ',p t k s ʃ h f x ɣ' 
-    bpcs += ',i iː ɪ y yː u uː ʉ'
-    bpcs += ',ʏ ø øː e eː ɛ ɛː ɛi œy ɑu ɔ o oː ə'
-    bpcs += ',a ɑ aː ɒː'
-    bpcs += ',i iː ɪ e eː ɛ ɛː ɛi œ œː a'
-    bpcs += ',ʉ ɨ ə ɜ ä'
-    bpcs += ',u uː ʊ o oː ɔ ɔː ɔi ɑ ɑː ɒ ɒː'
-    bpcs = [x.split() for x in bpcs.split(',')]
-    return dict(zip(names,bpcs))
-
-def ipa_to_bpc_dict():
-    '''return the broad phonetic class of a given IPA symbol.'''
-    d = {}
-    bpcs = make_bpcs()
-    ipas = []
-    for bpc in bpcs.values():
-        ipas.extend(bpc)
-    for ipa in ipas:
-        for bpc, phonemes in bpcs.items():
-            if not ipa in d.keys(): d[ipa] = []
-            if ipa in phonemes and bpc not in d[ipa]: d[ipa].append( bpc ) 
-    return d
-
 
 def make_phoneme_identifier(word, phoneme_index):
     return word.identifier + '_' + str(phoneme_index)
