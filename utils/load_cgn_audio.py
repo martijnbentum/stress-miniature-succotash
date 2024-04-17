82a0154e9dc1aca637c955aace58114edb91d05d
martijnbentum <bentummartijn@gmail.com>
2024-04-17 13:07:00 +0200

    moved generic audio functions to audio module

--- utils/load_cgn_audio.py
+++ utils/load_cgn_audio.py
@@ -2,29 +2,10 @@ from . import locations as locations
 import json
 from pathlib import Path
 from progressbar import progressbar
-import subprocess
+from utils import audio
 
 def cgn_audio_files(comps = 'abefghijklno',languages = ['nl','fl']):
     return locations.cgn_audio_files(comps,languages)
-
-def soxi_info(filename):
-    o = subprocess.run(['sox','--i',filename],stdout=subprocess.PIPE)
-    return o.stdout.decode('utf-8')
-
-def clock_to_duration_in_seconds(t):
-    hours, minutes, seconds = t.split(':')
-    s = float(hours) * 3600 + float(minutes) * 60 + float(seconds)
-    return s
-
-def soxinfo_to_dict(soxinfo):
-    x = soxinfo.split('\n')
-    d = {}
-    d['filename'] = x[1].split(': ')[-1].strip("'")
-    d['n_channels'] = int(x[2].split(': ')[-1])
-    d['sample_rate'] = int(x[3].split(': ')[-1])
-    t = x[5].split(': ')[-1].split(' =')[0]
-    d['duration'] = clock_to_duration_in_seconds(t)
-    return d
 
 def handle_audio_file(file_info):
     from text.models import Audio, Dataset, Language
@@ -34,7 +15,7 @@ def handle_audio_file(file_info):
     path = Path(filename)
     identifier = file_info['identifier']
     info = {'component':file_info['component'],'language':file_info['language']}
-    d = soxinfo_to_dict(soxi_info(filename))
+    d = audio.soxinfo_to_dict(audio.soxi_info(filename))
     d['identifier'] = identifier
     d['language'] = dutch
     d['info'] = json.dumps(info)
