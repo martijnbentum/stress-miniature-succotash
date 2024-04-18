80e470752788ff8f55ca374f137aa94f10022f31
martijnbentum <bentummartijn@gmail.com>
2024-04-18 17:01:53 +0200

    added phoneme map file load function; added dict methods to maus class to map between phoneme sets

--- utils/maus_phoneme_mapper.py
+++ utils/maus_phoneme_mapper.py
@@ -1,10 +1,43 @@
 from utils import locations
 
 def load_file(language):
-    pass
+    root_folder = locations.get_language_cv_root_folder(language)
+    filename = root_folder / f'maus_{language}_phoneme_map.txt'  
+    with open(filename, 'r') as f:
+        t = f.read().split('\n')
+    t = [x for x in t if not x.startswith('%') and x]
+    header, data = t[0].split('\t'), t[1:]
+    output = []
+    for line in data:
+        line = line.split('\t')
+        output.append(dict(zip(header, line)))
+    return output
 
 class Maus:
     def __init__(self, language):
         self.language = language
         self.raw = load_file(language)
-        self.phoneme_mapper = PhonemeMapper()
+
+    def maus_to_ipa(self):
+        d = {}
+        for line in self.raw:
+            if line['MAUS'] in '#,?,>,<,<usb>,<nib>,<p:>,<p>'.split(','): 
+                continue
+            d[line['MAUS']] = line['IPA']
+        return d
+
+    def sampa_to_ipa(self):
+        d = {}
+        for line in self.raw:
+            if line['MAUS'] in '#,?,>,<,<usb>,<nib>,<p:>,<p>'.split(','): 
+                continue
+            d[line['SAMPA']] = line['IPA']
+        return d
+
+    def ipa_to_lines(self):
+        d = {}
+        for line in self.raw:
+            if line['MAUS'] in '#,?,>,<,<usb>,<nib>,<p:>,<p>'.split(','): 
+                continue
+            d[line['IPA']] = line
+        return d
