9c158e94026be2c60ba5e6219cb8250562deb0e9
martijnbentum <bentummartijn@gmail.com>
2024-04-24 09:24:29 +0200

    added phoneme handling

--- utils/load_cv.py
+++ utils/load_cv.py
@@ -2,6 +2,8 @@ from utils import load_cv_audio
 from utils import load_cv_speakers
 from utils import load_cv_textgrids
 from utils import load_cv_words
+from utils import load_cv_phonemes
+
 
 def handle_language(language_name):
     print("Loading CV for language: " + language_name)
@@ -13,6 +15,9 @@ def handle_language(language_name):
     load_cv_textgrids.handle_language(language_name)
     print('handling words')
     load_cv_words.load_all_words_language(language_name)
+    print('handling phonemes')
+    load_cv_phonemes.handle_language(language_name)
+        
 
 def handle_languages(languages):
     for language in languages:
