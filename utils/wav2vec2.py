3c10909f5db351a32e7a134ff219711f0f911e78
martijnbentum <bentummartijn@gmail.com>
2024-05-03 18:54:39 +0200

    module to compute hidden states for words

---
+++ utils/wav2vec2.py
@@ -0,0 +1,37 @@
+import frame
+import load
+import pickle
+import to_vector
+
+def audio_to_vector(audio, model = None, gpu = False):
+    if not model: 
+        model = load.load_pretrained_model(gpu = gpu)
+    audio_filename = audio.filename
+    outputs = to_vector.filename_to_vector(audio_filename, model, gpu = gpu)
+    return outputs
+
+def handle_audio(audio, model = None, gpu = False, save_words = True,
+    output_directory = None):
+    if not output_directory:
+        output_directory = load.hidden_state_directory
+    outputs = audio_to_vector(audio, model, gpu)
+    words = audio.word_set.all()
+    output = []
+    for word in words:
+        start_time, end_time, identifier, name = word_to_info(word)
+        word_output= frame.extract_outputs_times(outputs, start_time, 
+            end_time)
+        word_output.identifier = identifier
+        word_output.name = name
+        if save_words:
+            filename = output_directory + identifier + ".pickle"
+            pickle.dump(word_output,open(filename, "wb"))
+        output.append(word_output)
+    return output
+
+def word_to_info(word):
+    start_time = word.start_time
+    end_time = word.end_time
+    identifier = word.identifier
+    name = word.word
+    return start_time, end_time, identifier, name
