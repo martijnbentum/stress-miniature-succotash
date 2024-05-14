ce26d84d336ea79e36238e0754cd2ea9170d23b2
martijnbentum <bentummartijn@gmail.com>
2024-05-14 16:30:08 +0200

    added section to handle english accents

--- load/load_cv.py
+++ load/load_cv.py
@@ -4,10 +4,12 @@ from load import load_cv_textgrids
 from load import load_cv_words
 from load import load_cv_phonemes
 from load import load_cv_syllables
+from load import add_english_accents
 
 
 def handle_language(language_name):
     print("Loading CV for language: " + language_name)
+    '''
     print('handling audio')
     load_cv_audio.handle_language(language_name)
     print('handling speakers')
@@ -16,6 +18,11 @@ def handle_language(language_name):
     load_cv_textgrids.handle_language(language_name)
     print('handling words')
     load_cv_words.load_all_words_language(language_name)
+    if language_name.lower() == 'english':
+        print('adding accents to speakers and words')
+        add_english_accents.add_accent_to_speakers()
+        add_english_accents.add_accent_to_words()
+    '''
     print('handling phonemes')
     load_cv_phonemes.handle_language(language_name)
     print('handling syllables')
