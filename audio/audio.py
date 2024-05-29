7aa4c49e7f965b00f11fda80207298d743404994
martijnbentum <bentummartijn@gmail.com>
2024-05-29 11:32:59 +0200

    added load and select audio functions"

--- audio/audio.py
+++ audio/audio.py
@@ -1,4 +1,35 @@
+import librosa
 import subprocess
+
+def time_to_samples(time, sr):
+    '''convert time to samples'''
+    return int(time * sr)
+
+def select_samples(signal, sr, start, end):
+    start = time_to_samples(start, sr)
+    end = time_to_samples(end, sr)
+    return signal[start:end]
+
+def item_to_samples(item, signal, sr):
+    start = item.start_time
+    end = item.end_time
+    return select_samples(signal, sr, start, end)
+
+def load_audio_file(file_path, sample_rate = 44100, start = 0.0, end = None):
+    '''load an audio file and return the signal and sample rate'''
+    if end:
+        duration = end - start
+    else: duration = None
+    signal, sr = librosa.load(file_path, sr=sample_rate, offset=start,
+        duration=duration)
+    return signal, sr
+
+def load_audio(audio, start = 0.0, end = None):
+    '''load audio file and return the signal and sample rate'''
+    file_path = audio.filename
+    sample_rate = audio.sample_rate
+    signal, sr = load_audio_file(file_path, sample_rate, start, end)
+    return signal, sr
 
 def soxi_info(filename):
     o = subprocess.run(['sox','--i',filename],stdout=subprocess.PIPE)
