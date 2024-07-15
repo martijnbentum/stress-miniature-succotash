107b6ec2572b81902b69c0fab36924709db0974b
martijnbentum <bentummartijn@gmail.com>
2024-07-15 12:56:40 +0200

    code clean up; name improvement

--- utils/info.py
+++ utils/info.py
@@ -2,11 +2,11 @@ import json
 from utils import locations
 from pathlib import Path
 
-def get_all_items_per_language(dataset_name = 'COMMON VOICE'):
-    get_words_per_language(dataset_name)
-    get_syllables_per_language(dataset_name)
-    get_phonemes_per_language(dataset_name)
-
+def get_all_item_counts_per_language(dataset_name = 'COMMON VOICE'):
+    '''prints number words syllables and phonemes per language.'''
+    get_word_counts_per_language(dataset_name)
+    get_syllables_counts_per_language(dataset_name)
+    get_phonemes_counts_per_language(dataset_name)
 
 def _get_item_counts_per_language(item_type = 'word', 
     dataset_name = 'COMMON VOICE'):
@@ -22,39 +22,21 @@ def _get_item_counts_per_language(item_type = 'word',
         items = getattr(language,item_type + '_set').filter(**d)
         print(f'{language}: {items.count()}')
 
-def get_words_per_language(dataset_name = 'COMMON VOICE'):
+def get_word_counts_per_language(dataset_name = 'COMMON VOICE'):
     _get_item_counts_per_language('word', dataset_name)
 
-def get_syllables_per_language(dataset_name = 'COMMON VOICE'):
+def get_syllables_counts_per_language(dataset_name = 'COMMON VOICE'):
     _get_item_counts_per_language('syllable', dataset_name)
 
-def get_phonemes_per_language(dataset_name = 'COMMON VOICE'):
+def get_phonemes_counts_per_language(dataset_name = 'COMMON VOICE'):
     _get_item_counts_per_language('phoneme', dataset_name)
 
-def _get_cv_items_of_language(language_name, item_type = 'word'):
-    from text.models import Language, Dataset, Word, Syllable, Phoneme
-    language = Language.objects.get(language__iexact = language_name)
-    dataset = Dataset.objects.get(name = 'COMMON VOICE')
-    if item_type == 'word': Item = Word
-    if item_type == 'syllable': Item = Syllable
-    if item_type == 'phoneme': Item = Phoneme
-    return Item.objects.filter(language = language, dataset = dataset)
-        
-def get_cv_words_of_language(language_name):
-    return _get_cv_items_of_language(language_name, 'word')
-        
-def get_cv_syllable_of_language(language_name):
-    return _get_cv_items_of_language(language_name, 'syllable')
-
-def get_cv_phoneme_of_language(language_name):
-    return _get_cv_items_of_language(language_name, 'phoneme')
-
 def load_cv_spk_ids(language_name):
-    with open(f'../{language_name}_cv_speaker_ids.json') as fin:
-        spk_ids = json.load(fin)
-    return spk_ids
+    '''wrapper for laod_or_make_cv_spk_id_json.'''
+    return load_or_make_cv_spk_id_json(language_name)
 
 def load_or_make_cv_spk_id_json(language_name, force_make = False):
+    '''make or load a json file with the speaker ids of a given language.'''
     language_name = language_name.lower()
     filename = Path(f'../{language_name}_cv_speaker_ids.json')
     if not force_make and filename.exists():
@@ -67,9 +49,8 @@ def load_or_make_cv_spk_id_json(language_name, force_make = False):
         json.dump(spk_id,fout)
     return spk_id
     
-def load_or_make_all_spk_ids_cv_dataset():
+def load_or_make_all_language_spk_ids():
     from text.models import Dataset 
-    dataset = Dataset.objects.get(name = 'COMMON VOICE')
     languages = dataset.language_str.split(', ')
     d = {}
     for language in languages:
@@ -78,6 +59,7 @@ def load_or_make_all_spk_ids_cv_dataset():
     return d
 
 def language_n_audio_dict():
+    '''return a dictionary with the number of audio files per language.'''
     if locations.language_naudios_dict.exists():
         with open(locations.language_naudios_dict) as fin:
             d = json.load(fin)
