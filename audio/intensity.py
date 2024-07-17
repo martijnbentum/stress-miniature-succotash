2128766a14a57852d13a7e5de99dd2560d6b9d52
martijnbentum <bentummartijn@gmail.com>
2024-07-17 12:49:57 +0200

    added function to create vowel stress intensity dict and plot the distributions

--- audio/intensity.py
+++ audio/intensity.py
@@ -1,6 +1,7 @@
 from audio import audio
 import numpy as np
-from progressbar import progressbar
+from utils import select
+from utils import plot_distributions
 
 def compute_praat_intensity(signal):
     '''
@@ -36,3 +37,42 @@ def handle_audio(audio):
         o = handle_word(word, signal)
         intensities.append(o)
     return intensities
+
+def _vowels_to_intensity(vowel_intensity_stress_dict):
+    d = vowel_intensity_stress_dict
+    if not d: d = make_vowel_duration_stress_dict('dutch')
+    output = {}
+    for k,v in d.items():
+        output[k] = [x.intensity for x in v]
+    return output
+
+def make_vowel_intensity_stress_dict(language_name = 'dutch', 
+    dataset_name = 'COMMON VOICE',minimum_n_syllables = 2,
+    max_n_items_per_speaker = None, vowel_stress_dict = None):
+    '''make dict mapping stress / no_stress to vowel intensities'''
+    if not vowel_stress_dict:
+        d = select.select_vowels(language_name, dataset_name,
+            minimum_n_syllables, max_n_items_per_speaker,
+            return_stress_dict = True)
+    else: d = vowel_stress_dict
+    d = _vowels_to_intensity(d)
+    return d
+
+def plot_stress_no_stress_distributions(intensities = None, new_figure = True,
+    minimal_frame = False, ylim = None, add_left = True, add_legend = True, 
+    bins = 240):
+    if not intensities: make_vowel_intensity_stress_dict()
+    kwargs = {
+        'value_dict':intensities,
+        'new_figure':new_figure,
+        'minimal_frame':minimal_frame,
+        'ylim':ylim,
+        'add_left':add_left,
+        'add_legend':add_legend,
+        'bins':bins,
+        'xlabel':'Intensity (dB)',
+        'xlim':(40,90),
+        }
+    plot_distributions.plot_stress_no_stress_distributions(**kwargs)
+
+
