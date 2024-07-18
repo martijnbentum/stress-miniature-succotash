1dcbf1a9b55d0c1dc99ddf089a3ea1be23e8e896
martijnbentum <bentummartijn@gmail.com>
2024-07-18 21:20:52 +0200

    added function to plot all acoustic feature distributions

--- utils/plot_distributions.py
+++ utils/plot_distributions.py
@@ -1,4 +1,13 @@
 from matplotlib import pyplot as plt
+from audio import duration
+from audio import formants
+from audio import frequency_band
+from audio import intensity
+from audio import pitch
+
+# circular imports for duration formants frequency_band intensity pitch
+# not sure if this is a major issue
+   
 
 def plot_stress_no_stress_distributions(value_dict, new_figure = True,
     minimal_frame = False, ylim = None, add_left = True, add_legend = True, 
@@ -28,3 +37,72 @@ def plot_stress_no_stress_distributions(value_dict, new_figure = True,
         ax.tick_params(left = False)
         ax.set_yticklabels([])
     plt.grid(alpha=0.3)
+
+def get_all_data(language_name = 'dutch', dataset_name = 'COMMON VOICE',
+    minimum_n_syllables = 2, max_n_items_per_speaker = None,
+    vowel_stress_dict = None):
+    if not vowel_stress_dict:
+        d = select.select_vowels(language_name, dataset_name,
+            minimum_n_syllables, max_n_items_per_speaker, 
+            return_stress_dict = True)
+    else: d = vowel_stress_dict
+    print('Processing durations')
+    durations = duration.make_vowel_duration_stress_dict(vowel_stress_dict = d)
+    print('Processing formants')
+    formant = formants.make_vowel_f1f2_distance_stress_dict(vowel_stress_dict=d)
+    print('Processing spectral tilt')
+    X,y = frequency_band.make_dataset(vowel_stress_dict = d)
+    clf, _, _ = frequency_band.train_lda(X, y, report = False)
+    print('Processing intensities')
+    intensities=intensity.make_vowel_intensity_stress_dict(vowel_stress_dict=d)
+    print('Processing pitch')
+    pitch_values = pitch.make_vowel_pitch_stress_dict(vowel_stress_dict=d)
+    data = {
+        'durations': durations,
+        'formants': formant,
+        'spectral_tilt': (X, y, clf),
+        'intensities': intensities,
+        'pitch': pitch_values,
+        }
+    return data
+
+def plot_all_acoustic_features(language_name = 'dutch',
+    dataset_name = 'COMMON VOICE',minimum_n_syllables = 2,
+    max_n_items_per_speaker = None, vowel_stress_dict = None, 
+    ylim = (0,50_000), data = None, row_index = 1, nrows = 1, 
+    new_figure = True):
+    '''plot all acoustic features'''
+    if not data:
+        data = get_all_data(language_name, dataset_name, minimum_n_syllables,
+            max_n_items_per_speaker, vowel_stress_dict)
+    plt.ion()
+    if new_figure: plt.figure()
+    plot_index = row_index * 5 - 5
+    print('plot index', plot_index)
+    plt.subplot(nrows,5,plot_index + 1)
+    intensity.plot_stress_no_stress_distributions(data['intensities'],
+        new_figure = False, minimal_frame = True, 
+        add_legend = False, ylim = ylim )
+    plt.subplot(nrows,5,plot_index +2)
+    duration.plot_stress_no_stress_distributions(data['durations'],
+        new_figure = False, minimal_frame = True, 
+        add_left = False, add_legend = False, ylim = ylim )
+    plt.subplot(nrows,5,plot_index + 3)
+    formants.plot_stress_no_stress_distributions(data['formants'],
+        new_figure = False, minimal_frame = True, 
+        add_left = False, add_legend = False, ylim = ylim )
+    plt.subplot(nrows,5,plot_index + 4)
+    pitch.plot_stress_no_stress_distributions(data['pitch'],
+        new_figure = False, minimal_frame = True, 
+        add_left = False, add_legend = False, ylim = ylim )
+    plt.subplot(nrows,5,plot_index + 5)
+    frequency_band.plot_lda_hist(*data['spectral_tilt'],
+        new_figure = False, minimal_frame = True, 
+        add_left = False, add_legend = True, ylim = ylim )
+    return data
+
+    
+    
+    
+    
+    
