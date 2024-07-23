280174eabfb49d4113916675d1143d1548441abb
martijnbentum <bentummartijn@gmail.com>
2024-07-23 09:40:07 +0200

    removed plot code instead use function in plot_distributions

--- utils/lda.py
+++ utils/lda.py
@@ -5,8 +5,12 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics import classification_report
 from sklearn.metrics import matthews_corrcoef
 from sklearn.model_selection import train_test_split
+from utils import plot_distributions
 
 def make_lda_report(clf, X_test, y_test):
+    '''make a classification report for an LDA classifier
+    with precision, recall, f1, and mcc scores
+    '''
     y_pred = clf.predict(X_test)
     cr = classification_report(y_test, y_pred)
     mcc = matthews_corrcoef(y_test, y_pred)
@@ -28,34 +32,19 @@ def train_lda(X, y, test_size = 0.33, report = True, random_state = 42):
 
 def plot_lda_hist(X, y, clf = None, new_figure = True, 
     minimal_frame = False, ylim = None, add_left = True, add_legend = True, 
-    bins = 380, xlabel = '', xlim = None):
+    bins = 380, xlabel = '', xlim = None, plot_density = False):
+    '''plot distribution of LDA scores for stress and no stress vowels'''
     X, y = np.array(X), np.array(y)
-
     if not clf:
         clf, _, _ = train_lda(X, y, report = False)
-
-    plt.ion()
-    if new_figure: plt.figure()
-    ax = plt.gca()
-    if minimal_frame:
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-    if ylim: plt.ylim(ylim)
-    if xlim: plt.xlim(xlim)
     tf = clf.transform(X)
-    plt.hist(tf[y==1], bins = 50, alpha=1, color = 'black',
-        label = 'stress')
-    plt.hist(tf[y==0], bins = 50, alpha=0.7, color = 'orange',
-        label = 'no stress')
+    d = {}
+    d['stress'] = tf[y==1].ravel()
+    d['no_stress'] = tf[y==0].ravel()
+    plot_distributions.plot_stress_no_stress_distributions(d, 
+        new_figure = new_figure, minimal_frame = minimal_frame,
+        ylim = ylim, add_left = add_left, add_legend = add_legend,
+        bins = bins, xlabel = xlabel, xlim = xlim, 
+        plot_density = plot_density)
 
-    if add_legend: plt.legend()
-    plt.xlabel(xlabel)
-    if add_left: plt.ylabel('Counts')
-    else:
-        ax.spines['left'].set_visible(False)
-        ax.tick_params(left = False)
-        ax.set_yticklabels([])
-    plt.grid(alpha=0.3)
-    if not xlabel: xlabel = 'Linear Discriminant score'
-    plt.xlabel(xlabel)
-    plt.show()
+
