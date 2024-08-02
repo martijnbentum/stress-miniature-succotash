01f0f46ac1d6d5efa98eec507712dd1e9863f566
martijnbentum <bentummartijn@gmail.com>
2024-08-02 18:07:49 +0200

    module to visualize hidden states

---
+++ utils/tsne.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import re
+
+def scale(hidden_states):
+    '''scale hidden states
+    '''
+    scaler = StandardScaler()
+    return scaler.fit_transform(hidden_states)
+
+def pca(hidden_states, n_components = 50, scale = False):
+    '''principal component analysis
+    '''
+    if scale:
+        hidden_states = scale(hidden_states)
+    pca = PCA(n_components = n_components)
+    return pca.fit_transform(hidden_states)
+
+def tsne(hidden_states, perplexity = 30, n_iter = 1000, random_state = 0,
+    apply_pca = False, pca_components = 50, scale_before_pca = False):
+    '''t-distributed stochastic neighbor embedding
+    '''
+    if apply_pca:
+        hidden_states = pca(hidden_states, n_components = pca_components, 
+            scale = scale_before_pca)
+    tsne = TSNE(n_components=2, perplexity=perplexity, n_iter=n_iter, 
+        random_state=random_state)
+    tsne_results = tsne.fit_transform(hidden_states)
+    return tsne_results
+
+def plot_tsne(tsne_hidden_states, labels = None, label_dict = None,
+    marker_dict = None, title = '', add_legend = True):
+    '''plot t-distributed stochastic neighbor embedding
+    '''
+    title = 't-SNE ' + title
+    plt.figure()
+    x = tsne_hidden_states[:,0]
+    y = tsne_hidden_states[:,1]
+    colors = plt.cm.tab20.colors
+    n = np.array(labels)
+    for label in label_dict.keys():
+        if label_dict:
+            label_name = label_dict[label]
+        else:
+            label_name = label
+        if marker_dict:
+            marker = marker_dict[label]
+        else:
+            marker = 'o'
+        color = colors[label]
+        print(label, label_name, color, marker)
+        index = np.where(n == label)
+        scatter = plt.scatter(x[index], y[index], label = label_name, 
+            color = color, marker = marker)
+    if add_legend:
+        plt.legend(title = 'Classes')
+    #plt.colorbar(scatter)
+    plt.title(title)
+    plt.show()
+    return scatter
+
+def old_plot_tsne(tsne_hidden_states, labels = None, label_dict = None,
+    title = '', add_legend = True):
+    '''plot t-distributed stochastic neighbor embedding
+    '''
+    title = 't-SNE' + title
+    plt.figure()
+    x = tsne_hidden_states[:,0]
+    y = tsne_hidden_states[:,1]
+    scatter = plt.scatter(x, y, c = labels, cmap = 'tab20')
+    handles, labels = scatter.legend_elements()
+    if label_dict:
+        labels = [int(re.search(r'\d+', label).group()) for label in labels]
+        labels = [label_dict[label] for label in labels]
+    if add_legend:
+        plt.legend(handles, labels, title = 'Classes')
+    #plt.colorbar(scatter)
+    plt.title(title)
+    plt.show()
+    return scatter
+
