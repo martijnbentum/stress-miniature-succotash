de4f18fc837beb595ae2d6bdc1bd608cb077208d
martijnbentum <bentummartijn@gmail.com>
2024-08-05 16:24:07 +0200

    abstracted functions to handle more situations; load_vowel_hidden_states -> load_segment_hidden_states that can handle onset vowel rime coda syllable and word; the plot function can also handle these types

--- datasets/word_phoneme_hidden_states.py
+++ datasets/word_phoneme_hidden_states.py
@@ -1,27 +1,30 @@
+from utils import load_hidden_states as lhs
+import numpy as np
+from utils import tsne
+
 def select_words(word_list, language_name = 'dutch'):
     from text.models import Word
     words = Word.objects.filter(word__in = word_list, 
         language__language__iexact = language_name)
     return list(words)
 
-def load_vowel_hidden_states(word_list, syllable_index = 0, mean = True,
-    hs = 'cnn'):
-    output = []
-    words = []
-    vowels = []
+def load_segment_hidden_states(word_list, syllable_index = 0, mean = True,
+    hs = 'cnn', segment_type = 'vowel'):
+    assert segment_type in ['onset', 'vowel', 'rime', 'coda', 'syllable','word']
+    output, words, segments = [], [], []
     for word in word_list:
-        syllable = word.syllables[syllable_index]
-        vowel = syllable.vowel
-        if not vowel: continue
-        vowel = vowel[0]
-        if hs == 'cnn':
-            hidden_states = vowel.cnn(mean = mean)
+        if segment_type == 'word': segment = word.phonemes
         else:
-            hidden_states = vowel.transformer(layer = hs, mean = mean)
+            syllable = word.syllables[syllable_index]
+            if segment_type == 'syllable': segment = syllable.phonemes
+            else: segment= getattr(syllable, segment_type)
+        if not segment: continue
+        hidden_states = lhs.phoneme_list_to_combined_hidden_states(
+            segment, hs = hs, mean = mean)
         output.append(hidden_states)
         words.append(word)
-        vowels.append(vowel)
-    return output, words, vowels
+        segments.append(segment)
+    return output, words, segments
 
 def word_to_lables(words):
     return [word.word for word in words]
@@ -31,8 +34,9 @@ def phoneme_to_labels(phonemes):
 
 def word_and_phoneme_to_labels(words, phonemes):
     output = []
-    for word, phoneme in zip(words, phonemes):
-        output.append(f'{phoneme.ipa} {word.word}')
+    for word, segments in zip(words, phonemes):
+        ipa = '-'.join([x.ipa for x in segments])
+        output.append(f'{ipa} {word.word}')
     return output
 
 def labels_to_numbers(labels):
@@ -62,6 +66,8 @@ def sample_words_by_type(words, max_tokens = 100):
 def make_phoneme_word_marker_dict(label_dict):
     output = {}
     markers = 'o', 's', '^', 'D', '<', '>', 'v', 'P', 'X', 'H'
+    markers += '+', 'x', '1', '2', '3', '4', '8', 'p', 'h', 'd'
+    markers += '|', '_', '.', ',', 'h'
     items = list(set([v.split(' ')[0] for v in label_dict.values()]))
     for k, v in label_dict.items():
         item = v.split(' ')[0]
@@ -76,12 +82,39 @@ def select_o_ij_dutch_words():
     output = sample_words_by_type(output)
     return output
 
-def plot_o_ij_words():
-    words = select_o_ij_dutch_words()
-    hidden_states, words, vowels = load_vowel_hidden_states(words)
-    labels = word_and_phoneme_to_labels(words)
-    tsne_hidden_states = tsne(hidden_states, perplexity = 10, n_iter = 1000, 
-        apply_pca = True, pca_components = 50, scale_before_pca = True)
-    plot_tsne(tsne_hidden_states, labels = labels)
+def plot_words(words = None, segment_types = ['vowel'], hs_types = [],
+    save = False, name = 'o_ij_dutch_words.pdf'):
+    from matplotlib import pyplot as plt
+    if not words: words = select_o_ij_dutch_words()
+    if not hs_types: hs_types = ['cnn', 11, 21]
+    n_columns = len(hs_types)
+    n_rows = len(segment_types)
+    fig, axes = plt.subplots(n_rows, n_columns, figsize = [18.6 ,  6*n_rows])
+    for column_index, hs in enumerate(hs_types):
+        for row_index, segment_type in enumerate(segment_types):
+            if len(segment_types) == 1 or len(hs_types) == 1:
+                ax = axes[column_index]
+            else: 
+                ax = axes[row_index, column_index]
+            title = f'{segment_type} {hs}'
+            hidden_states, w, segments= load_segment_hidden_states(words,
+                hs = hs, segment_type = segment_type)
+            labels = word_and_phoneme_to_labels(w,segments)
+            numbers, label_dict = labels_to_numbers(labels)
+            marker_dict = make_phoneme_word_marker_dict(label_dict)
+            X = tsne.tsne(np.array(hidden_states))
+            add_legend = True if column_index == len(hs_types) -1 else False
+            tsne.plot_tsne(X, numbers, label_dict, marker_dict, title, ax = ax,
+                add_legend = add_legend, legend_outside = True)
+    plt.show()
+    plt.tight_layout()
+    if save:
+        hs = '-'.join([str(x) for x in hs_types])
+        segment = '-'.join(segment_types)
+        filename = f'../figures/{segment}_{hs}_{name}'
+        plt.savefig(filename)
+    return fig
+    # plt.tight_layout(rect=[0, 0, 0.99, 1])
+
     
 
