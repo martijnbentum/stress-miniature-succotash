813a5aecd24a57fe5c6b29bc8e234e11ee31fe4a
martijnbentum <bentummartijn@gmail.com>
2024-10-30 12:48:47 +0100

    added function to make a list of artist song title

--- utils/mondegreen.py
+++ utils/mondegreen.py
@@ -1,4 +1,4 @@
-def load_mondegreen_dataset(return_dict=False):
+def load_mondegreen_dataset(return_dict=True):
     with open('../mondegreen/MondegreensFrench_utf8.txt') as f:
         t = f.read().split('\n')
         t = [x.split('\t') for x in t]
@@ -8,5 +8,14 @@ def load_mondegreen_dataset(return_dict=False):
     if return_dict:
         data = [{header[i]:x[i] for i in range(len(x))} for x in data]
     return header, data
+
+def make_artist_song_list():
+    header, data = load_mondegreen_dataset()
+    temp = [(x['Interpret'], x['Songtitel']) for x in data]
+    artist_song = []
+    for x in temp:
+        if x not in artist_song:
+            artist_song.append(x)
+    return artist_song
 
 
