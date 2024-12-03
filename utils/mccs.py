36c212ecaedee70380d6fd94dcbd42f3216d9fbc
martijnbentum <bentummartijn@gmail.com>
2024-12-03 12:33:58 +0100

    updated plot mccs results

--- utils/mccs.py
+++ utils/mccs.py
@@ -4,30 +4,88 @@ import numpy as np
 from pathlib import Path
 from utils import stats
 
-classifier_order = 'duration,intensity,pitch,formant,spectral-tilt'
-classifier_order += ',combined-features,codevector,cnn,5,11,17,23'
-classifier_order = classifier_order.split(',')
-
-def load_results():
+language_names = ['dutch', 'english', 'german', 'polish', 'hungarian']
+classifier_order = ['duration', 'intensity', 'pitch', 'formant',
+    'spectral-tilt', 'combined-features', 'codevector', 'cnn', 5, 11, 
+    17, 23]
+        
+def load_results(return_stats = True):
     with open('../results.json') as f:
         d = json.load(f)
+    if return_stats: return results_to_stats(d)
     return d
 
-def _plot_language_mccs(mcc_result_dict, language_name = 'dutch', 
-    new_figure = True, offset = 0, color = 'black'):
+def _result_to_identifier(result):
+    language_name = result['language_name']
+    layer = result['layer']
+    name = result['name']
+    if not name: return f'{language_name}-{layer}'
+    return f'{language_name}-{layer}-{name}'
+
+def results_to_stats(results):
+    output = {}
+    for result in results:
+        identifier = _result_to_identifier(result)
+        language_name = result['language_name']
+        layer = result['layer']
+        mcc = result['mcc']
+        name = result['name']
+        if identifier not in output: 
+            output[identifier] = {'language_name': language_name, 
+                'layer': layer, 'name': name, 'mccs': [mcc]}
+        else:
+            output[identifier]['mccs'].append(mcc)
+    o = []
+    for key, item in output.items():
+        item['mean'], item['sem'], item['ci'] = stats.compute_mean_sem_ci(
+            item['mccs'])
+        item['mccs'] = [round(x, 3) for x in item['mccs']]
+        item['identifier'] = key
+        o.append(item)
+    return o 
+
+def filter_cross_lingual(cross_lingual = True):
+    results = load_results()
+    output = []
+    for result in results:
+        if cross_lingual:
+            if 'other-language-' in result['name']:
+                output.append(result)
+        else:
+            if not 'other-language-' in result['name']:
+                output.append(result)
+    return output
+        
+
+def filter_language(language_name, cross_lingual = False):
+    results = load_results()
+    results = filter_cross_lingual(cross_lingual)
+    output = []
+    for result in results:
+        if language_name == result['language_name']:
+            output.append(result)
+    output = sorted(output, key = lambda x: classifier_order.index(x['layer']))
+    return output
+
+def plot_language_mccs(language_name = 'dutch', new_figure = True, 
+    offset =0, color = 'black', plot_grid = True):
+    results = filter_language(language_name)
     plt.ion()
     if new_figure: plt.figure()
     plt.ylim(0,1)
-    d = mcc_result_dict[language_name]
-    x = np.arange(len(d)) + offset
-    means = [d[layer]['mean'] for layer in d.keys()]
-    cis = [d[layer]['ci'] for layer in d.keys()]
+    x = np.arange(len(results)) + offset
+    x_tick_names = [result['layer'] for result in results]
+    x_tick_names[x_tick_names.index('combined-features')] = 'combined'
+    means = [result['mean'] for result in results]
+    cis = [result['ci'] for result in results]
     plt.errorbar(x, means, yerr = cis, label = language_name, markersize = 12,
-        color = color, fmt = ',', elinewidth = 2.5, capsize = 9, 
+        color = color, fmt = ',', elinewidth = 1.5, capsize = 3, 
         capthick = 1.5)
     if abs(x[0]) < 0.01:
-        plt.xticks(x, d.keys(), rotation = 45)
+        plt.xticks(x, x_tick_names, rotation = 90)
     plt.ylabel('Matthews correlation coefficient')
+    if plot_grid: plt.grid(alpha = .5) 
+    plt.tight_layout()
 
 def _plot_vertical_lines(n_categories, width = 1):
     x = np.arange(n_categories)
@@ -37,25 +95,21 @@ def _plot_vertical_lines(n_categories, width = 1):
             plt.axvspan(x[i] - offset, x[i] + offset, color = 'grey', 
                 alpha = .1) 
     
-def plot_mccs(mcc_result_dict = None, new_figure = True, 
-    language_names = []):
-    if not mcc_result_dict:
-        with open('../results/mccs_stress_vowel.json') as f:
-            mcc_result_dict = json.load(f)
+def plot_mccs(new_figure = True, languages= []):
     plt.ion()
     if new_figure: plt.figure()
+    if not languages: languages = language_names
     plt.ylim(0,1)
-    n_languages = len(mcc_result_dict.keys())
+    n_languages = len(languages)
     delta_offset = 1 / n_languages
     offset = 0 - delta_offset if n_languages % 2 == 0 else 0 - delta_offset * 2
     colors = plt.get_cmap('tab10').colors
-    if not language_names: language_names = mcc_result_dict.keys()
     for i,language in enumerate(language_names):
         print('plotting', language)
-        _plot_language_mccs(mcc_result_dict, language, new_figure = False, 
-            offset = offset, color = colors[i])
+        plot_language_mccs(language, new_figure = False, 
+            offset = offset, color = colors[i], plot_grid = False)
         offset += delta_offset
-    n_categories = len(mcc_result_dict[language].keys())
+    n_categories = len(classifier_order)
     _plot_vertical_lines(n_categories)
     plt.legend()
     plt.grid(alpha = .5, axis = 'y')
