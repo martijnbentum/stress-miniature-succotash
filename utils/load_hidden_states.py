77ca96f8304c254bbf40ef085c242900f359dac2
martijnbentum <bentummartijn@gmail.com>
2024-12-04 17:18:06 +0100

    hidden states need model as input / no longer property of word / syllable / phoneme but a method, default model = 'pretrained-xlsr

--- utils/load_hidden_states.py
+++ utils/load_hidden_states.py
@@ -4,11 +4,11 @@ from w2v2_hidden_states import frame
 
 hidden_state_indices = 1,3,5,7,9,11,13,15,17,19,21,23,24
 
-def load_word_hidden_states(word):
-    return shs.load_word_hidden_states(word)
+def load_word_hidden_states(word, model_name = 'pretrained-xlsr'):
+    return shs.load_word_hidden_states(word, model_name = model_name)
 
 def phoneme_list_to_combined_hidden_states(phoneme_list, hs = 'cnn',
-    mean = False):
+    mean = False, model_name = 'pretrained-xlsr'):
     '''load hidden states from list of phonemes'''
     hidden_states_list = []
     for phoneme in phoneme_list:
@@ -17,9 +17,10 @@ def phoneme_list_to_combined_hidden_states(phoneme_list, hs = 'cnn',
         elif hs == 'codevector':
             hidden_states = phoneme.codevector(mean = False)
         elif hs == 'hidden_states':
-            hidden_states = phoneme.hidden_states
+            hidden_states = phoneme.hidden_states(model_name = model_name)
         else:
-            hidden_states = phoneme.transformer(layer = hs, mean = False)
+            hidden_states = phoneme.transformer(layer = hs, mean = False,
+                model_name = model_name)
         if hidden_states is None: continue
         hidden_states_list.append(hidden_states)
     if len(hidden_states_list) == 0: return None
@@ -45,10 +46,12 @@ def _replace_none_hidden_states(hidden_states):
         if h is None:
             hidden_states.hidden_states[i] = np.zeros((0,0,0))
 
-def load_syllable_hidden_states(syllable, word_hidden_states = None):
+def load_syllable_hidden_states(syllable, word_hidden_states = None, 
+    model_name = 'pretrained-xlsr'):
     '''load syllable hidden states from word hidden states'''
     if not word_hidden_states:
-        word_hidden_states = load_word_hidden_states(syllable.word)
+        word_hidden_states = load_word_hidden_states(syllable.word,
+            model_name = model_name)
     _replace_none_hidden_states(word_hidden_states)
     start_time = syllable.start_time
     end_time = syllable.end_time
@@ -62,11 +65,13 @@ def load_syllable_hidden_states(syllable, word_hidden_states = None):
     syllable_hidden_states = o
     return syllable_hidden_states
 
-def load_phoneme_hidden_states(phoneme, word_hidden_states = None):
+def load_phoneme_hidden_states(phoneme, word_hidden_states = None, 
+    model_name = 'pretrained-xlsr'):
     '''load phoneme hidden states from word hidden states'''
     if not word_hidden_states:
         word = phoneme.word
-        word_hidden_states = load_word_hidden_states(word)
+        word_hidden_states = load_word_hidden_states(word, 
+            model_name = model_name)
     if not word_hidden_states: return None
     _replace_none_hidden_states(word_hidden_states)
     start_time = phoneme.start_time
@@ -85,8 +90,8 @@ def load_phoneme_hidden_states(phoneme, word_hidden_states = None):
     return phoneme_hidden_states
         
 
-def add_to_word(word, add_lower = False):
-    hs = load_word_hidden_states(word)
+def add_to_word(word, add_lower = False, model_name = 'pretrained-xlsr'):
+    hs = load_word_hidden_states(word, model_name = model_name)
     if not hs is None: 
         _replace_none_hidden_states(hs)
     word._hidden_states = hs
