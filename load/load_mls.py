92e830ca8d4c5c3274181bac4cc4d6e01d1fa6fd
martijnbentum <bentummartijn@gmail.com>
2024-12-22 09:56:54 +0100

    updated mls file

--- load/load_mls.py
+++ load/load_mls.py
@@ -1,11 +1,13 @@
-from load import load_bpcs
+from load import load_bpc
 from load import load_mls_audio
 from load import load_mls_speakers
 from load import load_mls_textgrids
 from load import load_mls_words
 from load import load_mls_phonemes
-from load import load_mls_syllables
-from load import load_mls_stress
+# from load import load_mls_syllables
+# from load import load_mls_stress
+from load import load_languages
+from load import load_datasets
 from utils import locations
 
 
@@ -14,16 +16,18 @@ def handle_language(language_name):
     print('handling bpcs')
     load_bpc.handle_bpcs()
     print('handling audio')
-    load_cv_audio.handle_language(language_name)
+    load_mls_audio.handle_language(language_name)
     print('handling speakers')
-    load_cv_speakers.handle_language(language_name)
+    load_mls_speakers.handle_language(language_name)
     print('handling textgrids')
-    load_cv_textgrids.handle_language(language_name)
+    load_mls_textgrids.handle_language(language_name)
     print('handling words')
-    load_cv_words.load_all_words_language(language_name)
+    load_mls_words.load_all_words_language(language_name)
     print('handling phonemes')
-    load_cv_phonemes.handle_language(language_name)
+    load_mls_phonemes.handle_language(language_name)
+    '''
     print('handling syllables')
-    load_cv_syllables.handle_language(language_name)
+    load_mls_syllables.handle_language(language_name)
     print('handling stress')
-    load_cv_stress.handle_language(language_name)
+    load_mls_stress.handle_language(language_name)
+    '''
