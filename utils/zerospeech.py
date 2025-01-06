dc50e28186007695afdea7a76729446091ba89c4
martijnbentum <bentummartijn@gmail.com>
2025-01-06 15:03:32 +0100

    bug fixes

--- utils/zerospeech.py
+++ utils/zerospeech.py
@@ -2,13 +2,23 @@ from progressbar import progressbar
 from text.models import Textgrid, Language
 from utils import locations
 
-def read_manifest(language_name = 'English'):
+def handle_language(language_name):
+    print('making sentences')
+    sentences = make_sentences(language_name)
+    print('making words')
+    words = make_words(sentences, language_name)
+    save_words(language_name, words)
+    print('making phonemes')
+    phonemes = make_phonemes(sentences, language_name)
+    save_phonemes(language_name, phonemes)
+
+def read_manifest(language_name):
     directory = locations.get_language_mls_root_folder(language_name)
     with open(directory / 'train.tsv','r') as f:
         t = [x.split('\t')[0].split('/')[-1] for x in f.read().split('\n') if x]
     return t
 
-def read_meta_data(language_name = 'English'):
+def read_meta_data(language_name):
     directory = locations.get_language_mls_root_folder(language_name)
     with open(directory / 'metainfo.txt','r') as f:
         t = f.read().split('\n')
@@ -30,8 +40,8 @@ def read_meta_data(language_name = 'English'):
     header, data = output[0], output[1:]
     return header, data
 
-def gender_dict():
-    header, data = read_meta_data()
+def gender_dict(language_name):
+    header, data = read_meta_data(language_name)
     d = {}
     for line in data:
         d[line[0]] = line[1]
@@ -39,81 +49,88 @@ def gender_dict():
 
     
 
-def get_textgrids(language_name = 'English'):
+def get_textgrids(language_name):
     language= Language.objects.get(language__iexact=language_name)
     return Textgrid.objects.filter(audio__language=language)
 
-def make_sentences():
-    textgrids = get_textgrids()
+def make_sentences(language_name): 
+    manifest = read_manifest(language_name)
+    textgrids = get_textgrids(language_name)
     sentences = []
+    error = []
     for textgrid in progressbar(textgrids):
-        sentences.extend(textgrid_to_sentence(textgrid))
+        try: temp = textgrid_to_sentence(textgrid, manifest)
+        except ValueError: error.append(textgrid)
+        else:sentences.extend(temp)
+    if error:
+        for tg in error:
+            print(f'Error with {tg.identifier}')
+        print('Errors:',len(error))
     return sentences
 
-def make_words(sentences = None):
+def make_words(sentences = None,language_name = ''):
     words = []
-    if not sentences: sentences = make_sentences()
+    if not sentences: sentences, error = make_sentences(language_name)
     for sentence in progressbar(sentences):
         for i, word in enumerate(sentence.words):
             words.append(Word(word, sentence, i))
     return words
 
-def make_phonemes(sentences = None):
+def make_phonemes(sentences = None,language_name = ''):
     phonemes = []
     if not sentences: sentences = make_sentences()
     for sentence in progressbar(sentences):
         for i, phoneme in enumerate(sentence.phonemes):
             phonemes.append(Phoneme(phoneme, sentence, i))
-    return phonemes
+    return phonemes 
 
-def save_phonemes(phonemes = None):
+def save_phonemes(language_name, phonemes = None):
     header = 'audio_filename\tstart_time\tend_time\tduration\tphoneme'
     header += '\tprevious_phoneme\tnext_phoneme\tspeaker_id\toverlap'
     header += '\tin_pretraining'
     o = [header]
-    if not phonemes: phonemes = make_phonemes()
+    if not phonemes: phonemes = make_phonemes(language_name = language_name)
     for phoneme in phonemes:
         o.append(phoneme.line)
-    with open('../news_phonemes_zs.tsv','w') as f:
+    with open(f'../{language_name}_mls_phonemes_zs.tsv','w') as f:
         f.write('\n'.join(o))
 
-def save_words(words = None):
+def save_words(language_name, words = None):
     header = 'audio_filename\tstart_time\tend_time\tduration\ttext\tspeaker_id'
     header += '\toverlap\tin_pretraining'
     o = [header]
-    if not words: words = make_words()
+    if not words: words = make_words(language_name = language_name)
     for word in words:
         o.append(word.line)
-    with open('../news_words_zs.tsv','w') as f:
+    with open(f'../{language_name}_mls_words_zs.tsv','w') as f:
         f.write('\n'.join(o))
 
-def save_sentences(sentences = None):
+def save_sentences(language_name, sentences = None):
     header = 'audio_filename\tstart_time\tend_time\tduration\ttext\tidentifier'
     header += '\tspeaker_id\tin_pretraining'
     o = [header]
-    if not sentences: sentences = make_sentences()
+    if not sentences: sentences = make_sentences(language_name = language_name)
     for sentence in sentences:
         o.append(sentence.line)
-    with open('../news_sentences_zs.tsv','w') as f:
+    with open(f'../{language_name}_mls_sentences_zs.tsv','w') as f:
         f.write('\n'.join(o))
     
 
 
-def textgrid_to_sentence(textgrid):
+def textgrid_to_sentence(textgrid, manifest):
     index = 0
-    temp = list(textgrid.audio.word_set.all())
-    sentence = Sentence(temp, textgrid, index)
+    word_list= list(textgrid.audio.word_set.all())
+    sentence = Sentence(word_list, textgrid, index, manifest)
     sentences = [sentence]
     return sentences
 
 
-def check_sentence_in_pretraining(sentence):
+def check_sentence_in_pretraining(sentence, manifest):
     filename = sentence.audio_filename.split('/')[-1]
-    manifest = read_manifest()
     return filename in manifest
 
 class Sentence:
-    def __init__(self, words, textgrid, index):
+    def __init__(self, words, textgrid, index, manifest):
         self.words = words
         self.start_time = 0 # words[0].start_time
         self.end_time = textgrid.audio.duration # words[-1].end_time
@@ -125,14 +142,15 @@ class Sentence:
         self.duration = self.end_time - self.start_time
         speakers = textgrid.speakers.all()
         if len(speakers) > 1: raise ValueError('More than one speaker')
+        if len(speakers) == 0: 
+            print(f'No speakers for {self.textgrid.identifier}')
+            raise ValueError('No speakers')
         self.speaker = speakers[0]
+        self.gender = self.speaker.gender
         speaker_id = self.speaker.identifier
-        d = gender_dict()
-        if speaker_id in d: gender = d[speaker_id]
-        else: gender = None
-        self.speaker_id = f'{speaker_id}_{gender}' 
+        self.speaker_id = f'{speaker_id}_{self.gender}' 
         self.sentence = ' '.join([w.word for w in words])
-        self.in_pretraining = check_sentence_in_pretraining(self)
+        self.in_pretraining = check_sentence_in_pretraining(self, manifest)
 
     @property
     def phonemes(self):
@@ -166,7 +184,7 @@ class Word:
         self.start_time = word.start_time - sentence.start_time
         self.end_time = word.end_time - sentence.start_time
         self.duration = self.end_time - self.start_time
-        self.speaker_id = str(word.speaker).replace(' ','_')
+        self.speaker_id = sentence.speaker_id
 
     def __repr__(self):
         m = f'{self.filename} {self.speaker_id} {self.duration:.3f}' 
@@ -195,18 +213,18 @@ class Phoneme:
         self.start_time = phoneme.start_time - sentence.start_time
         self.end_time = phoneme.end_time - sentence.start_time
         self.duration = self.end_time - self.start_time
-        self.speaker_id = str(phoneme.word.speaker).replace(' ','_')
+        self.speaker_id = sentence.speaker_id
         self.overlap = phoneme.word.overlap
 
     @property
     def previous_phoneme(self):
         if self.index == 0: return 'SOS'
-        return self.sentence.phonemes[self.index - 1].phoneme
+        return self.sentence.phonemes[self.index - 1].phoneme.ipa
 
     @property
     def next_phoneme(self):
         if self.index == len(self.sentence.phonemes) - 1: return 'EOS'
-        return self.sentence.phonemes[self.index + 1].phoneme
+        return self.sentence.phonemes[self.index + 1].phoneme.ipa
 
     @property
     def line(self):
