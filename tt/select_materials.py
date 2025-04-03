e33d77a36726d705ed2f1afe8299a3a7788fd1e6
martijnbentum <bentummartijn@gmail.com>
2025-04-03 19:25:18 +0200

    plot functions for phoneme and word counts

--- tt/select_materials.py
+++ tt/select_materials.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import gaussian_kde
@@ -9,6 +10,7 @@ def load_audios():
     return audios
 
 def load_words(audios = None):
+    if audios is None: audios = load_audios()
     words = []
     for audio in audios:
         words.extend(list(audio.word_set.all()))
@@ -55,3 +57,25 @@ def plot_word_durations(words = None, max_value = 1):
     plt.show()
     return words
 
+def plot_phoneme_counts(phonemes = None):
+    if phonemes is None: phonemes = load_phonemes()
+    ipa = Counter([x.ipa for x in phonemes])
+    plt.figure(figsize=(12,3))
+    plt.bar(range(len(ipa)), [x[1] for x in ipa.most_common()])
+    plt.xticks(range(len(ipa)), [x[0] for x in ipa.most_common()])
+    plt.tight_layout()
+    plt.show()
+    return phonemes
+
+def plot_word_counts(words = None, top_n_words = 70):
+    if words is None: words = load_words()
+    n = top_n_words
+    c = Counter([x.word for x in words])
+    plt.figure(figsize=(12,3))
+    plt.bar(range(n), [x[1] for x in c.most_common()[:n]])
+    plt.xticks(range(n), [x[0] for x in c.most_common()[:n]], rotation = 90)
+    plt.title(f'Word Counts for top {n} words')
+    plt.grid(alpha = 0.5)
+    plt.tight_layout()
+    plt.show()
+    return words
