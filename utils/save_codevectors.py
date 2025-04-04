7d0f3047cbfa63dc5b4df47ddc71c02bb2a248a5
martijnbentum <bentummartijn@gmail.com>
2025-04-04 17:34:37 +0200

    updated function name of load_word_hidden_states to load_word_hidden_state_frames

--- utils/save_codevectors.py
+++ utils/save_codevectors.py
@@ -68,7 +68,7 @@ def load_word_codebook_indices(word, model_name = 'pretrained-xlsr'):
     return ci
 
 def _word_to_codebook_indices(word, model_pt, model_name = 'pretrained-xlsr'):
-    frames= lhs.load_word_hidden_states(word, model_name = model_name)
+    frames= lhs.load_word_hidden_state_frames(word, model_name = model_name)
     outputs = frames.outputs
     if outputs is None: return None
     codebook_indices = codebook.outputs_to_codebook_indices(outputs, model_pt)
