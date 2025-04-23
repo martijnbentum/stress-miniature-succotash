3c3cd72be44dfb56454a29ef0ab9d8780358aa2d
martijnbentum <bentummartijn@gmail.com>
2025-04-23 11:02:56 +0200

    removed functions to get checkpoint paths but where in fact retrieving model names; replaced them with pointers to checkpoint module

--- tt/compute_codevectors.py
+++ tt/compute_codevectors.py
@@ -5,25 +5,18 @@ from progressbar import progressbar
 from . import select_materials
 from . import model_names
 from . import step_list
+from . import model_checkpoints 
 
 
-def get_checkpoints():
-    checkpoints = link_audio_to_model.get_model_folders()
-    return checkpoints
+def get_checkpoints(language = 'nl'):
+    return model_checkpoints.get_model_checkpoints(language)
+    
 
-def language_step_to_checkpoint(language, step):
-    checkpoints = get_checkpoints()
-    for checkpoint in checkpoints:
-        l = link_audio_to_model.dir_to_language(checkpoint)
-        if l not in ['nl', 'en', 'ns']:
-            continue
-        s = link_audio_to_model.dir_to_step(checkpoint)
-        if l == language and s == step:
-            return checkpoint
-    return None
+def language_step_to_model_checkpoint(language, step):
+    return model_checkpoints.language_step_model_checkpoint(language, step)
 
 def language_step_to_model_pt(language, step, gpu = False):
-    checkpoint = language_step_to_checkpoint(language, step)
+    checkpoint = language_step_to_model_checkpoint(language, step)
     if checkpoint is None:
         return None
     return load.load_model_pt(checkpoint, gpu = gpu)
@@ -48,28 +41,30 @@ def save_word_codevectors(word, model_pt, model_name, overwrite = False):
     save_codevectors.save_word_codebook_indices(word, model_pt, model_name,
         overwrite = overwrite)
 
-def handle_all_languages(words = None):
+def handle_all_languages(words = None, overwrite = False):
     if words is None:
         words = select_materials.load_words()
     for language in ['nl', 'en', 'ns']:
         print(f'language: {language}')
         handle_language(language, words)
 
-def handle_language(language = 'nl', words = None):
+def handle_language(language = 'nl', words = None, overwrite = False, 
+    steps = None):
     if words is None:
         words = select_materials.load_words()
-    steps = model_names.steps
+    if steps is None:
+        steps = model_names.steps
     for step in steps:
-        checkpoint = language_step_to_checkpoint(language, step)
+        checkpoint = language_step_to_model_checkpoint(language, step)
         print(f'checkpoint: {checkpoint}')
-        handle_checkpoint(checkpoint, words)
+        handle_checkpoint(checkpoint, words, overwrite = overwrite)
 
-def handle_checkpoint(checkpoint, words = None):
+def handle_checkpoint(checkpoint, words = None, overwrite = False):
     if words is None:
         words = select_materials.load_words()
-    language = link_audio_to_model.dir_to_language(checkpoint)
-    step = link_audio_to_model.dir_to_step(checkpoint)
+    language = model_checkpoints.model_checkpoint_to_language(checkpoint)
+    step = model_checkpoints.model_checkpoint_to_step(checkpoint)
     model_name = language_step_to_model_name(language, step)
-    model_pt = language_step_to_model_pt(language, step)
+    model_pt = load.load_model_pt(checkpoint, gpu = False)
     for word in progressbar(words):
-        save_word_codevectors(word, model_pt, model_name)
+        save_word_codevectors(word, model_pt, model_name, overwrite = overwrite)
