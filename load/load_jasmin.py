bb354bca0fd03cc8f935803e1c2301d574d460c8
martijnbentum <bentummartijn@gmail.com>
2025-05-02 15:41:09 +0200

    updated all calls to jasmin

--- load/load_jasmin.py
+++ load/load_jasmin.py
@@ -8,16 +8,16 @@ from utils import locations
 
 
 def handle_cgn():
-    print("Loading CGN")
+    print("Loading JASMIN")
     print('handling bpcs')
     load_bpc.handle_bpcs()
     print('handling audio')
     load_jasmin_audio.handle_audio_files()
     print('handling speakers')
-    load_cgn_speakers.add_all_speakers()
+    load_jasmin_speakers.add_all_speakers()
     print('handling textgrids')
-    load_cgn_textgrids.load_in_all_awd_textgrids()
+    load_jasmin_textgrids.load_in_all_awd_textgrids()
     print('handling words')
-    load_cgn_words.load_all_words()
+    load_jasmin_words.load_all_words()
     print('handling phonemes')
-    load_cgn_phonemes.load_all_phonemes()
+    load_jasmin_phonemes.load_all_phonemes()
